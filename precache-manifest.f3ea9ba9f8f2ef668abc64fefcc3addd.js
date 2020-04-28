e562bcab47793d65891038eb30dec3a83f2c35f6
Marcin Olszowski <olszyna38@gmail.com>
2020-04-28 21:06:51 +0200

    Updates

---
+++ precache-manifest.f3ea9ba9f8f2ef668abc64fefcc3addd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5b8bd648a52592d6187ea615a686cc",
+    "url": "/NBP-kurs-walut/index.html"
+  },
+  {
+    "revision": "cb2ad19c137e30861023",
+    "url": "/NBP-kurs-walut/static/js/2.34333531.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NBP-kurs-walut/static/js/2.34333531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dadcf7d8d70e28bfecc2",
+    "url": "/NBP-kurs-walut/static/js/main.6d240dd6.chunk.js"
+  },
+  {
+    "revision": "6b5112a672243e1cafad",
+    "url": "/NBP-kurs-walut/static/js/runtime-main.fb484ea4.js"
+  }
+]);
