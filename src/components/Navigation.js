94556e86cca309c1e38f967ee49d2224d9b26e02
Marcin Olszowski <olszyna38@gmail.com>
2020-05-20 22:24:42 +0200

    Add tolltip

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 import { Span, Input, Button } from "../theme/CommonStyle";
 
@@ -21,6 +23,7 @@ const NavigationTitle = styled.p`
 `;
 
 const NavigationLabel = styled.label`
+  position: relative;
   flex-basis: 53%;
   display: flex;
   justify-content: space-between;
@@ -28,6 +31,88 @@ const NavigationLabel = styled.label`
 `;
 
 const NavigationDateInfo = styled(Span)``;
+
+const NavigationInfoIcon = styled.div`
+  position: absolute;
+  left: 200px;
+  top: -2px;
+  font-size: 1.3rem;
+
+  color: #aaaaaa; /* #696969; */
+  display: inline-block;
+  margin-top: -10px;
+  padding-top: 10px;
+  text-decoration: none;
+
+  &:before {
+    animation: hidden ease-in-out 0.2s 1 forwards;
+    background: rgba(0, 0, 0, 0.8);
+    border-radius: 5px;
+    color: #fff;
+    content: attr(data-tooltip);
+    font-size: 1.2rem;
+    opacity: 0;
+    padding: 12px;
+    position: absolute;
+    top: 152px;
+    left: -10px;
+    transform: translateY(-100%);
+    visibility: hidden;
+    width: 400px;
+    letter-spacing: 0.3px;
+    line-height: 20px;
+  }
+
+  &:after {
+    animation: hidden ease-in-out 0.2s 1 forwards;
+    border: solid transparent;
+    border-bottom-color: rgba(0, 0, 0, 0.8);
+    border-width: 0px 10px 10px 10px;
+    content: "";
+    height: 0px;
+    left: 0px;
+    opacity: 0;
+    position: absolute;
+    top: 38px;
+    visibility: hidden;
+    width: 0px;
+  }
+
+  &:hover:before,
+  &:hover:after {
+    animation: show ease-in-out 0.4s 1 forwards;
+  }
+
+  @keyframes show {
+    0% {
+      opacity: 0;
+      visibility: hidden;
+    }
+    1% {
+      opacity: 0;
+      visibility: visible;
+    }
+    100% {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+
+  @keyframes hidden {
+    0% {
+      opacity: 1;
+      visibility: visible;
+    }
+    99% {
+      opacity: 0;
+      visibility: visible;
+    }
+    100% {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+`;
 
 const NavigationInput = styled(Input)`
   &::-webkit-clear-button {
@@ -47,6 +132,7 @@ const Navigation = ({
   getCurrencyCalculator,
   handleDate,
   tableDate,
+  endDate,
 }) => {
   const a = "a";
   const c = "c";
@@ -68,6 +154,9 @@ const Navigation = ({
               : `${today.getMonth() + 1}`}
             .{today.getFullYear()}, wybierz datę:
           </NavigationDateInfo>
+          <NavigationInfoIcon data-tooltip="Tabela kursów średnich aktualizowana jest w każdy dzień roboczy między godziną 11:45 i 12:15. Tabela kursów kupna i sprzedaży aktualizowana jest w każdy dzień roboczy między godziną 7:45 i 8:15.">
+            <FontAwesomeIcon icon={faInfoCircle} />
+          </NavigationInfoIcon>
           <NavigationInput
             type="date"
             value={tableDate}
@@ -76,13 +165,13 @@ const Navigation = ({
           />
         </NavigationLabel>
       </NavigationHeading>
-      <NavigationButton onClick={() => getCurrencyTable(a, tableDate)}>
+      <NavigationButton onClick={() => getCurrencyTable(a, endDate)}>
         Średnie
       </NavigationButton>
-      <NavigationButton onClick={() => getCurrencyTable(c, tableDate)}>
+      <NavigationButton onClick={() => getCurrencyTable(c, endDate)}>
         Kupna i sprzedaży
       </NavigationButton>
-      <NavigationButton onClick={() => getCurrencyCalculator(tableDate)}>
+      <NavigationButton onClick={() => getCurrencyCalculator(endDate)}>
         Kalkulator walutowy
       </NavigationButton>
     </NavigationContainer>
