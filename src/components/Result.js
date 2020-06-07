ea23ebc9caff59c7e1a6406a8703c896bf3648df
Marcin Olszowski <olszyna38@gmail.com>
2020-06-07 20:36:34 +0200

    Add country flags

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Span } from "../theme/CommonStyle";
+import { Image, Span } from "../theme/CommonStyle";
 
 const ResultContainer = styled.div`
   height: 30%;
@@ -14,6 +14,13 @@ const ResultBigInfo = styled.p`
   font-size: ${(props) => props.theme.fontSize.xl};
   font-weight: ${(props) => props.theme.bold600};
   letter-spacing: 0.5px;
+`;
+
+const Flag = styled(Image)`
+  display: inline-block;
+  width: 45px;
+  height: 30px;
+  margin: 0 20px;
 `;
 
 const ResultBigInfoSpan = styled.span`
@@ -37,10 +44,12 @@ const Result = ({
       {!isDisabled && result && (
         <>
           <ResultBigInfo>
+            <Flag src={require(`../images/${convertFrom}.png`)} alt="flag" />
             {(amount * 1).toFixed(2)}{" "}
             <ResultBigInfoSpan>{convertFrom}</ResultBigInfoSpan> ={" "}
             {result.toFixed(2)}{" "}
             <ResultBigInfoSpan>{convertTo}</ResultBigInfoSpan>
+            <Flag src={require(`../images/${convertTo}.png`)} alt="flag" />
           </ResultBigInfo>
           <ResultLittleInfoSpan>
             1 {convertFrom} = {(1 / (amount / result)).toFixed(4)} {convertTo}{" "}
@@ -51,10 +60,12 @@ const Result = ({
       {isDisabled && result && (
         <>
           <ResultBigInfo>
+            <Flag src={require(`../images/${convertFrom}.png`)} alt="flag" />
             {(amount * 1).toFixed(2)}{" "}
             <ResultBigInfoSpan>{convertFrom}</ResultBigInfoSpan> ={" "}
             {result.toFixed(2)}{" "}
             <ResultBigInfoSpan>{convertTo}</ResultBigInfoSpan>
+            <Flag src={require(`../images/${convertTo}.png`)} alt="flag" />
           </ResultBigInfo>
           <ResultLittleInfoSpan>
             1 {convertFrom} = {(result / amount).toFixed(4)} {convertTo} według
